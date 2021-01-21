edeb38a68c9ce602e55dcf8231ae95ce55c4c7d4
Şennur Özen <sennurozennn@gmail.com>
2021-01-21 20:10:27 +0300

    21 ocak

---
+++ Koleksiyonlar/Koleksiyonlar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Miray", "Şennur", "Fatma" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            isimler.Add("Eda");
+            Console.WriteLine(isimler[3]);
+            Console.WriteLine(isimler[0]);
+        }
+    }
+}
